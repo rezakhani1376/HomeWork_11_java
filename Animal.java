84d6c397b06f357d70a36e139710cd78df4473e6
reza <reza.khani1998@gmail.com>
2019-06-30 15:40:43 +0430

    create class BreadType

--- Animal.java
+++ Animal.java
@@ -1,8 +1,17 @@
 
-
 abstract class Animal {
+    private String name;
+    public Animal(String name) {
+        this.name = name;
+    }
 
-    String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
 
